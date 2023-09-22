6c4eb5f39b3c02f12452d7d517e254293caaec8e
parakhchoudhary1208 <0808cs181119.ies@ipsacadeny.org>
2023-09-22 09:51:10 +0530

    Media Query File

--- js/script.js
+++ js/script.js
@@ -6,3 +6,18 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+const hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', function() {
+  document.querySelector('.menu-mob').style.left = '0';
+})
+
+const infoOverlay = document.querySelector('.info-overlay');
+const nutriOverlay = document.querySelector('.nutri-overlay');
+infoOverlay.addEventListener('mouseover', function() {
+  nutriOverlay.style.opacity = '1';
+})
+infoOverlay.addEventListener('mouseout', function() {
+  nutriOverlay.style.opacity = '0';
+})
