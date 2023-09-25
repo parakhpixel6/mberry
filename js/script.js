df64b72f6f109cb532f80beb1c2e7e5ee3dc48e4
parakhchoudhary1208 <0808cs181119.ies@ipsacadeny.org>
2023-09-25 09:43:56 +0530

    Footer Initialise

--- js/script.js
+++ js/script.js
@@ -9,15 +9,36 @@ var swiper = new Swiper(".mySwiper", {
 
 
 const hamburger = document.querySelector('.hamburger');
+const close_nav = document.querySelector('.close_nav');
 hamburger.addEventListener('click', function() {
   document.querySelector('.menu-mob').style.left = '0';
 })
+close_nav.addEventListener('click', function() {
+  document.querySelector('.menu-mob').style.left = '100%';
+})
 
-const infoOverlay = document.querySelector('.info-overlay');
-const nutriOverlay = document.querySelector('.nutri-overlay');
-infoOverlay.addEventListener('mouseover', function() {
-  nutriOverlay.style.opacity = '1';
-})
-infoOverlay.addEventListener('mouseout', function() {
-  nutriOverlay.style.opacity = '0';
-})
+const infoOverlays = document.querySelectorAll('.info-overlay');
+const nutriOverlays = document.querySelectorAll('.nutri-overlay');
+
+infoOverlays.forEach((infoOverlay, index) => {
+  infoOverlay.addEventListener('mouseleave', () => {
+    setTimeout(() => {
+      nutriOverlays[index].style.opacity = 0; // Change back to 0 or your desired value
+    }, 3000);
+  });
+
+  nutriOverlays[index].addEventListener('mouseleave', () => {
+    nutriOverlays[index].style.opacity = 0;
+  });
+
+  infoOverlay.addEventListener('mouseenter', () => {
+    nutriOverlays[index].style.opacity = 1;
+  });
+
+  if (nutriOverlays[index].style.opacity === 1) {
+    nutriOverlays[index].addEventListener('mouseenter', () => {
+      nutriOverlays[index].style.opacity = 1;
+    });
+  }
+
+});
